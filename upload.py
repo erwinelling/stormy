07e23c2dae44df52f35c8715cc056d71a6dec863
Erwin Elling <erwinelling@Erwins-MacBook-Pro.local>
2016-10-05 23:51:56 +0200

    Fixed upload counter

--- upload.py
+++ upload.py
@@ -136,8 +136,8 @@ try:
                     # remove .notuploaded file
                     os.remove(not_uploaded_file)
                     count +=1
-                    logger.debug("Uploaded %s file(s)", count)
 
+logger.debug("Uploaded %s file(s)", count)
 except HTTPError, e:
     logger.error(e)
     pass
